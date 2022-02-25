9dde7339e90afee103e64e93c8bff4cd22423b05
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-02-25 12:44:33 +0600

    display all info

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ const randomUsers = async() => {
         // convert to json
         const user = await response.json();
 
-        console.log(user.results[0]);
+        console.log(user.results[0].email);
         
         // display field
         const displayUser = document.getElementById('display-user');
@@ -50,8 +50,7 @@ const randomUsers = async() => {
                     class="nav d-flex justify-content-center align-items-center gap-5"
                   >
                     <svg
-                    onClick="name()"
-                      class=""
+                      id="display-name"
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 448 512"
                     >
@@ -59,32 +58,42 @@ const randomUsers = async() => {
                         d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z"
                       />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                      <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                    <svg 
+                      id="display-email"
+                      xmlns="http://www.w3.org/2000/svg" 
+                      viewBox="0 0 512 512">
                       <path
                         d="M493.6 163c-24.88-19.62-45.5-35.37-164.3-121.6C312.7 29.21 279.7 0 256.4 0H255.6C232.3 0 199.3 29.21 182.6 41.38c-118.8 86.25-139.4 101.1-164.3 121.6C6.75 172 0 186 0 200.8v263.2C0 490.5 21.49 512 48 512h416c26.51 0 48-21.49 48-47.1V200.8C512 186 505.3 172 493.6 163zM303.2 367.5C289.1 378.5 272.5 384 256 384s-33.06-5.484-47.16-16.47L64 254.9V208.5c21.16-16.59 46.48-35.66 156.4-115.5c3.18-2.328 6.891-5.187 10.98-8.353C236.9 80.44 247.8 71.97 256 66.84c8.207 5.131 19.14 13.6 24.61 17.84c4.09 3.166 7.801 6.027 11.15 8.478C400.9 172.5 426.6 191.7 448 208.5v46.32L303.2 367.5z"
                       />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                      <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                    <svg 
+                      id="display-dob"
+                      xmlns="http://www.w3.org/2000/svg" 
+                      viewBox="0 0 448 512">
                       <path
                         d="M152 64H296V24C296 10.75 306.7 0 320 0C333.3 0 344 10.75 344 24V64H384C419.3 64 448 92.65 448 128V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V128C0 92.65 28.65 64 64 64H104V24C104 10.75 114.7 0 128 0C141.3 0 152 10.75 152 24V64zM48 248H128V192H48V248zM48 296V360H128V296H48zM176 296V360H272V296H176zM320 296V360H400V296H320zM400 192H320V248H400V192zM400 408H320V464H384C392.8 464 400 456.8 400 448V408zM272 408H176V464H272V408zM128 408H48V448C48 456.8 55.16 464 64 464H128V408zM272 192H176V248H272V192z"
                       />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                      <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                    <svg 
+                      id="display-location"
+                      xmlns="http://www.w3.org/2000/svg" 
+                      viewBox="0 0 576 512">
                       <path
                         d="M408 120C408 174.6 334.9 271.9 302.8 311.1C295.1 321.6 280.9 321.6 273.2 311.1C241.1 271.9 168 174.6 168 120C168 53.73 221.7 0 288 0C354.3 0 408 53.73 408 120zM288 152C310.1 152 328 134.1 328 112C328 89.91 310.1 72 288 72C265.9 72 248 89.91 248 112C248 134.1 265.9 152 288 152zM425.6 179.8C426.1 178.6 426.6 177.4 427.1 176.1L543.1 129.7C558.9 123.4 576 135 576 152V422.8C576 432.6 570 441.4 560.9 445.1L416 503V200.4C419.5 193.5 422.7 186.7 425.6 179.8zM150.4 179.8C153.3 186.7 156.5 193.5 160 200.4V451.8L32.91 502.7C17.15 508.1 0 497.4 0 480.4V209.6C0 199.8 5.975 190.1 15.09 187.3L137.6 138.3C140 152.5 144.9 166.6 150.4 179.8H150.4zM327.8 331.1C341.7 314.6 363.5 286.3 384 255V504.3L192 449.4V255C212.5 286.3 234.3 314.6 248.2 331.1C268.7 357.6 307.3 357.6 327.8 331.1L327.8 331.1z"
                       />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                      <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                    <svg 
+                      id="display-phone"
+                      xmlns="http://www.w3.org/2000/svg" 
+                      viewBox="0 0 512 512">
                       <path
                         d="M18.92 351.2l108.5-46.52c12.78-5.531 27.77-1.801 36.45 8.98l44.09 53.82c69.25-34 125.5-90.31 159.5-159.5l-53.81-44.04c-10.75-8.781-14.41-23.69-8.974-36.47l46.51-108.5c6.094-13.91 21.1-21.52 35.79-18.11l100.8 23.25c14.25 3.25 24.22 15.8 24.22 30.46c0 252.3-205.2 457.5-457.5 457.5c-14.67 0-27.18-9.968-30.45-24.22l-23.25-100.8C-2.571 372.4 5.018 357.2 18.92 351.2z"
                       />
                     </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                      <!--! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
+                    <svg 
+                      id="display-password"
+                      xmlns="http://www.w3.org/2000/svg" 
+                      viewBox="0 0 448 512">
                       <path
                         d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z"
                       />
@@ -93,7 +102,37 @@ const randomUsers = async() => {
                 </div>
               </div>
             </div>
-            `
+            `;
+
+            document.getElementById('display-name').onclick = function() {
+              document.getElementById('pretext-value').innerText = 'Hi, My name is';
+              document.getElementById('value').innerText = `${user.results[0].name.title + ' '+ user.results[0].name.first + ' ' + user.results[0].name.last}`;
+            }
+
+            document.getElementById('display-email').onclick = function() {
+              document.getElementById('pretext-value').innerText = 'My email address is';
+              document.getElementById('value').innerText = `${user.results[0].email}`;
+            }
+
+            document.getElementById('display-dob').onclick = function() {
+              document.getElementById('pretext-value').innerText = 'My birthday is';
+              document.getElementById('value').innerText = `${user.results[0].dob.date.slice(0, 10)}`;
+            }
+
+            document.getElementById('display-location').onclick = function() {
+              document.getElementById('pretext-value').innerText = 'My address is';
+              document.getElementById('value').innerText = `${user.results[0].location.city + ', ' + user.results[0].location.state + ', ' + user.results[0].location.country}`;
+            }
+
+            document.getElementById('display-phone').onclick = function() {
+              document.getElementById('pretext-value').innerText = 'My phone number is';
+              document.getElementById('value').innerText = `${user.results[0].phone}`;
+            }
+
+            document.getElementById('display-password').onclick = function() {
+              document.getElementById('pretext-value').innerText = 'My password is';
+              document.getElementById('value').innerText = `${user.results[0].login.password}`;
+            }
         }
 
     } catch (error) {
